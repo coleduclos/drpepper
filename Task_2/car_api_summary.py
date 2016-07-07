2675f524c8d0d3f4fec6514551c045a1ae0d1420
Cole Duclos <cole.duclos@gmail.com>
2016-07-07 18:59:25 -0400

    final submit for task 2

--- Task_2/car_api_summary.py
+++ Task_2/car_api_summary.py
@@ -7,6 +7,8 @@ out_json = {}
 out_file = "cars.json"
 bucket_name = "bugcrowd-infrastructure-interview"
 bucket_key = "coleduclos/cars.json"
+car_model_key = "car_model"
+car_count_key = "car_count"
 
 # Fetch the json data from the url
 print "Fetching data from %s" % url
@@ -21,15 +23,15 @@ for car in json_data['Makes']:
 	# Check if make country already exists in dictionary
 	if make_country not in out_json:
 		out_json[make_country] = {}
-		out_json[make_country]['car_models'] = [make_display]
-		out_json[make_country]['car_count'] =  1
+		out_json[make_country][car_model_key] = [make_display]
+		out_json[make_country][car_count_key] =  1
 	else:
-		out_json[make_country]['car_models'].append(make_display)
-		out_json[make_country]['car_count'] = out_json[make_country]['car_count'] + 1
+		out_json[make_country][car_model_key].append(make_display)
+		out_json[make_country][car_count_key] = out_json[make_country][car_count_key] + 1
 
 # Loop through each country in out_json and order cars (descending)
 for country in out_json:
-	out_json[country]['car_models'] = sorted(out_json[country]['car_models'], reverse=True) 
+	out_json[country][car_model_key] = sorted(out_json[country][car_model_key], reverse=True) 
 
 # Write the json object to the out_file
 print "Wrting results to %s" % out_file
@@ -40,3 +42,5 @@ fd.write(json.dumps(out_json, indent=4, sort_keys=True))
 print "Uploading results to AWS S3 bucket %s/%s" % (bucket_name, bucket_key)
 s3 = boto3.resource('s3')
 s3.Bucket(bucket_name).put_object(Key=bucket_key, Body=json.dumps(out_json, indent=4, sort_keys=True))
+
+print "FINISHED"
